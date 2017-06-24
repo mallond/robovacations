12625109761173842cd34e5e7b6e1e892d9d1bdb
david mallon <mallond@gmail.com>
2017-06-24 17:31:33 -0500

    draft

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Component } from 'react';
 
+import Header from './header';
 import SearchBar from '../containers/search_bar';
 import WeatherList from '../containers/weather_list';
+
 
 export default class App extends Component {
   render() {
     return (
       <div>
+        <Header/>
         <SearchBar />
         <WeatherList />
       </div>
---
+++ src/components/header.js
@@ -0,0 +1,20 @@
+/**
+ * Created by dm on 6/24/17.
+ */
+
+import React, { Component} from 'react';
+
+
+
+class Header extends Component {
+
+  render () {
+    return (
+      <div className="header">
+        <img className='img-fluid' src={'images/sexyrobots.jpg'}></img>
+      </div>
+    );
+  }
+}
+
+export default Header;
