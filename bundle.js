0a5eace53dd51975f32bfc041d4971147268c6a1
david mallon <mallond@gmail.com>
2017-06-25 10:57:23 -0500

    add bundle

--- bundle.js
+++ bundle.js
@@ -38772,6 +38772,65 @@
 	        "footer",
 	        null,
 	        _react2.default.createElement(
+	          "ol",
+	          { className: "wpp-list" },
+	          _react2.default.createElement(
+	            "li",
+	            null,
+	            " ",
+	            _react2.default.createElement(
+	              "a",
+	              { href: "http://www.telegraph.co.uk/business/2017/06/25/narcissistic-bosses-biggest-threat-robot-revolution/", title: "Why humans must accept that robots make better decisions", clasNames: "wpp-post-title", target: "_blank" },
+	              "Why humans must accept that robots make better decisions"
+	            ),
+	            " "
+	          ),
+	          _react2.default.createElement(
+	            "li",
+	            null,
+	            " ",
+	            _react2.default.createElement(
+	              "a",
+	              { href: "https://qz.com/1010828/is-it-unethical-to-design-robots-to-resemble-humans/", title: "Is it unethical to design robots to resemble humans?", className: "wpp-post-title", target: "_blank" },
+	              "Is it unethical to design robots to resemble humans?"
+	            ),
+	            " "
+	          ),
+	          _react2.default.createElement(
+	            "li",
+	            null,
+	            " ",
+	            _react2.default.createElement(
+	              "a",
+	              { href: "http://www.newsobserver.com/news/business/biz-columns-blogs/article156940409.html", title: "Robots with human features may unsettle some people. But they\u2019re coming.", className: "wpp-post-title", target: "__blank" },
+	              "Robots with human features may unsettle some people. But they\u2019re coming."
+	            ),
+	            " "
+	          ),
+	          _react2.default.createElement(
+	            "li",
+	            null,
+	            " ",
+	            _react2.default.createElement(
+	              "a",
+	              { href: "https://www.nbcnews.com/mach/tech/what-rise-sentient-robots-will-mean-human-beings-ncna773146", title: "What the Rise of Sentient Robots Will Mean for Human Beings", className: "wpp-post-title", target: "_blank" },
+	              "What the Rise of Sentient Robots Will Mean for Human Beings"
+	            ),
+	            " "
+	          ),
+	          _react2.default.createElement(
+	            "li",
+	            null,
+	            " ",
+	            _react2.default.createElement(
+	              "a",
+	              { href: "https://www.forbes.com/sites/washingtonbytes/2017/06/13/how-congress-can-beat-back-the-robots/#7f85d5b15570", title: "How Congress Can Beat Back The Robotsa\xA0", className: "wpp-post-title", target: "_blank" },
+	              "How Congress Can Beat Back The Robots\xA0"
+	            ),
+	            " "
+	          )
+	        ),
+	        _react2.default.createElement(
 	          "p",
 	          null,
 	          "Posted by: David Mallon"
